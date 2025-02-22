1b29db5ae13556e1a9cb6036c23a94360c13ca74
Srinivasan Venkataramanan <svenka3@gmail.com>
2025-02-21 22:35:07 -0800

    Update af_no_global_vars.py

--- src/rules/af_no_global_vars.py
+++ src/rules/af_no_global_vars.py
@@ -1,4 +1,8 @@
-
+# ----------------------------------------------------
+# SPDX-FileCopyrightText: AsFigo Technologies, UK
+# SPDX-FileCopyrightText: VerifWorks, India
+# SPDX-License-Identifier: MIT
+# ----------------------------------------------------
 from af_lint_rule import AsFigoLintRule
 import logging
 

56f8eccbd66168dcca0f9c34001a3ab500287db0
Himank Gangwal <himankgangwal007@gmail.com>
2025-04-20 18:25:00 -0700

    Package check updated with ancestor checking

--- src/rules/af_package_check.py
+++ src/rules/af_package_check.py
@@ -5,6 +5,7 @@
 # Author: Himank Gangwal, April 04, 2025
 # ----------------------------------------------------
 from af_lint_rule import AsFigoLintRule
+import anytree
 import logging
 
 class PackageCheckRule(AsFigoLintRule):
@@ -13,13 +14,29 @@ class PackageCheckRule(AsFigoLintRule):
     def apply(self, filePath: str, data: AsFigoLintRule.VeribleSyntax.SyntaxData):
         for classNode in data.tree.iter_find_all(filter_={"tag": "kClassDeclaration"}, iter_=AsFigoLintRule.VeribleSyntax.PostOrderTreeIterator):
             className = self.getClassName(classNode)
-            # print(f"C:{className}:{classNode.depth}")
-            if(classNode.depth > 1):
-                # print(f"class {className} may have package")
-                continue
-            else:
+            
+            ancestor = anytree.util.commonancestors(classNode)
+
+            print(f"{className}:{ancestor}:{len(ancestor)}")
+            found_package = False
+            if len(ancestor) > 1:
+                if "package" in ancestor[1].text:
+                    found_package = True
+                    print(f"{className}:{ancestor[1].text}")
+
+            if found_package == False:
+                print(f"{className}:Error no package")
                 # print(f"class {className} is missing a package")
                 message = f"Found a class without a package:\n {classNode.text} \n in file: {filePath}"
                 self.linter.logViolation("R115",message)
+
+            # # print(f"C:{className}:{classNode.depth}")
+            # if(classNode.depth > 1):
+            #     # print(f"class {className} may have package")
+            #     continue
+            # else:
+            #     # print(f"class {className} is missing a package")
+            #     message = f"Found a class without a package:\n {classNode.text} \n in file: {filePath}"
+            #     self.linter.logViolation("R115",message)
 
 
--- tests/test_cl_package_check.sv
+++ tests/test_cl_package_check.sv
@@ -6,15 +6,12 @@
      local int lvar_i1;
    endclass : c
 
-
-package myc3pkg;
 class c3;
   protected int state_i1;
   rand int rand_i1;
   protected int lvar_i1;
   protected int prot_i1;
 endclass : c3
-endpackage // missing lable
 
 package my_pkg;  
   class Transaction;  
